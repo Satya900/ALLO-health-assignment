5094c312236f13f354f5fa7376163ffa30b0ec86
Satyabrata Mohanty <mohantysatyabrata900@gmail.com>
2025-10-08 01:14:40 +0530

    Initialize backend for clinic front desk system
    Set up backend structure with Express, MongoDB, and Mongoose. Added environment configuration, basic server setup, and placeholder files for controllers, models, middleware, and routes. Updated dependencies and included initial requirements and documentation files.

--- Backend/index.js
+++ Backend/index.js
@@ -3,15 +3,24 @@ const express = require('express');
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
+const cors = require('cors')
+const mongoose = require('mongoose')
 
+app.use(cors())
+app.use(express.json())
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected"))
+  .catch((err) => console.error("❌ MongoDB Error:", err));
 
 app.get('/', (req,res)=>{
     return res.status(200).json({
-        msg:"Backend is runing fine"
+        msg:"Backend is running fine"
     })
 })
 
 app.listen(PORT, ()=>{
-    console.log(`Serveris running on PORT ${PORT}`);
+    console.log(`Server is running on PORT ${PORT}`);
     
 })
